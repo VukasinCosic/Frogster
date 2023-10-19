4be1d70c1e44f24a5ba9eee6cfce72e188abb413
Vukasin Cosic <vulevu997@gmail.com>
2023-10-19 17:53:13 +0200

    Fixed crashes due to access violations.

--- game.cpp
+++ game.cpp
@@ -25,26 +25,8 @@ float player_half_size_x = 5, player_half_size_y = 5;
 float player_pos_x = 0.f;
 float player_pos_y = -45.f;
 float object_half_size_y = 5.f;
-float object_row_half_size_x[] = {  15.f,
-									10.f,
-									20.f};
-
-u32 object_row_color[] = {  0x0000ff,
-							0x00ff00,
-							0xffff00 };
-
-objectToDraw enemyObjects[] = { {addEnemyObject(-110.f,-35.f,1,1)},
-								{addEnemyObject(-90.f,-25.f,2,2)},
-								{addEnemyObject(-70.f,-15.f,3,3)},
-								{addEnemyObject(-50.f,-5.f,1,1)},
-								{addEnemyObject(-30.f,5.f,2,2)},
-								{addEnemyObject(-10.f,15.f,3,3)},
-								{addEnemyObject(10.f,25.f,1,1)},
-								{addEnemyObject(30.f,35.f,2,2)}
-							 };
-
 internal bool
-colision(float p1x, float p1y, float hs1x, float p2x, float p2y, float hs2x) 
+colision(float p1x, float p1y, float hs1x, float p2x, float p2y, float hs2x)
 {
 	return (p1x + hs1x > p2x - hs2x &&
 		p1x - hs1x < p2x + hs2x &&
@@ -54,7 +36,7 @@ colision(float p1x, float p1y, float hs1x, float p2x, float p2y, float hs2x)
 #define enemyObjSpeed(n) enemyObjects[n-1].pos_x
 #define objColision(n) player_pos_x, player_pos_y, player_half_size_x, enemyObjects[(n-1)].pos_x, enemyObjects[(n-1)].pos_y, enemyObjects[(n-1)].half_size_x
 
-internal void simulate_game(Input* input, float dt/*, float* object_row_half_size_x, u32* object_row_color, objectToDraw* enemyObjects*/)
+internal void simulate_game(Input* input, float dt, float* object_row_half_size_x, u32* object_row_color, objectToDraw* enemyObjects)
 {
 	clear_screen(0x000000);
 
@@ -74,81 +56,75 @@ internal void simulate_game(Input* input, float dt/*, float* object_row_half_siz
 	{
 		player_pos_x += 10.f;
 	}
-		
-		if(level == 1)
+
+	if (level == 1)
+	{
+		enemyObjSpeed(1) += speed * 3.f * dt;
+		enemyObjSpeed(2) += speed * 4.f * dt;
+		enemyObjSpeed(3) += speed * 3.f * dt;
+		enemyObjSpeed(4) += speed * 5.f * dt;
+		enemyObjSpeed(5) += speed * 4.f * dt;
+		enemyObjSpeed(6) += speed * 5.f * dt;
+		enemyObjSpeed(7) += speed * 3.f * dt;
+		enemyObjSpeed(8) += speed * 6.f * dt;
+
+		for (int i = 0; i < sizeof(enemyObjects); i++)
 		{
-			enemyObjSpeed(1) += speed * 3.f * dt;
-			enemyObjSpeed(2) += speed * 4.f * dt;
-			enemyObjSpeed(3) += speed * 3.f * dt;
-			enemyObjSpeed(4) += speed * 5.f * dt;
-			enemyObjSpeed(5) += speed * 4.f * dt;
-			enemyObjSpeed(6) += speed * 5.f * dt;
-			enemyObjSpeed(7) += speed * 3.f * dt;
-			enemyObjSpeed(8) += speed * 6.f * dt;
-
-			for (int i = 0; i < sizeof(enemyObjects); i++)
-			{
-				backToScreenRight(i);
-			}
-
-			for (int i = 0; i < sizeof(enemyObjects); i++)
-			{
-				checkColision(i);
-			}
-
-			for (int i = 0; i < sizeof(enemyObjects); i++)
-			{
-				draw_rect(enemyObjects[i].pos_x, enemyObjects[i].pos_y, enemyObjects[i].half_size_x, enemyObjects[i].half_size_y, enemyObjects[i].color);
-			}
-
-			draw_rect(player_pos_x, player_pos_y, player_half_size_x, player_half_size_y, 0xff0000);
-			if (player_pos_y > 39.f && level < 2)
-			{
-				player_pos_x = 0.f;
-				player_pos_y = -45.f;
-				//float pos_move = 0;
-				//for (int i = 0; i < sizeof(enemyObjects); i++)
-				//{
-				//	enemyObjects[i].pos_x = 110.f - pos_move;
-				//	pos_move += 20.f;
-				//}
-				level++;
-			}
+			backToScreenRight(i);
 		}
-		
-		if (level == 2)
+
+		for (int i = 0; i < sizeof(enemyObjects); i++)
 		{
-			enemyObjSpeed(1) -= speed * 4.f * dt;
-			enemyObjSpeed(2) -= speed * 5.f * dt;
-			enemyObjSpeed(3) -= speed * 4.f * dt;
-			enemyObjSpeed(4) -= speed * 6.f * dt;
-			enemyObjSpeed(5) -= speed * 5.f * dt;
-			enemyObjSpeed(6) -= speed * 6.f * dt;
-			enemyObjSpeed(7) -= speed * 4.f * dt;
-			enemyObjSpeed(8) -= speed * 7.f * dt;
-
-			for (int i = 0; i < sizeof(enemyObjects); i++)
-			{
-				backToScreenLeft(i);
-			}
-
-			for (int i = 0; i < sizeof(enemyObjects); i++)
-			{
-				checkColision(i);
-			}
-			for (int i = 0; i < sizeof(enemyObjects); i++)
-			{
-				draw_rect(enemyObjects[i].pos_x, enemyObjects[i].pos_y, enemyObjects[i].half_size_x, enemyObjects[i].half_size_y, enemyObjects[i].color);
-			}
-
-			draw_rect(player_pos_x, player_pos_y, player_half_size_x, player_half_size_y, 0xff0000);
-			if (player_pos_y > 39.f && level < 3)
-			{
-				player_pos_x = 0.f;
-				player_pos_y = -45.f;
-				level++;
-			}
-			if (level > 2)
-				running = false;
+			checkColision(i);
 		}
+
+		for (int i = 0; i < sizeof(enemyObjects); i++)
+		{
+			draw_rect(enemyObjects[i].pos_x, enemyObjects[i].pos_y, enemyObjects[i].half_size_x, enemyObjects[i].half_size_y, enemyObjects[i].color);
+		}
+
+		draw_rect(player_pos_x, player_pos_y, player_half_size_x, player_half_size_y, 0xff0000);
+		if (player_pos_y > 39.f && level < 2)
+		{
+			player_pos_x = 0.f;
+			player_pos_y = -45.f;
+			level++;
+		}
+	}
+
+	if (level == 2)
+	{
+		enemyObjSpeed(1) -= speed * 4.f * dt;
+		enemyObjSpeed(2) -= speed * 5.f * dt;
+		enemyObjSpeed(3) -= speed * 4.f * dt;
+		enemyObjSpeed(4) -= speed * 6.f * dt;
+		enemyObjSpeed(5) -= speed * 5.f * dt;
+		enemyObjSpeed(6) -= speed * 6.f * dt;
+		enemyObjSpeed(7) -= speed * 4.f * dt;
+		enemyObjSpeed(8) -= speed * 7.f * dt;
+
+		for (int i = 0; i < sizeof(enemyObjects); i++)
+		{
+			backToScreenLeft(i);
+		}
+
+		for (int i = 0; i < sizeof(enemyObjects); i++)
+		{
+			checkColision(i);
+		}
+		for (int i = 0; i < sizeof(enemyObjects); i++)
+		{
+			draw_rect(enemyObjects[i].pos_x, enemyObjects[i].pos_y, enemyObjects[i].half_size_x, enemyObjects[i].half_size_y, enemyObjects[i].color);
+		}
+
+		draw_rect(player_pos_x, player_pos_y, player_half_size_x, player_half_size_y, 0xff0000);
+		if (player_pos_y > 39.f && level < 3)
+		{
+			player_pos_x = 0.f;
+			player_pos_y = -45.f;
+			level++;
+		}
+		if (level > 2)
+			running = false;
+	}
 }
--- win32_platform.cpp
+++ win32_platform.cpp
@@ -22,39 +22,57 @@ LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPa
 	LRESULT result = 0;
 	switch (uMsg)
 	{
-		case WM_SIZE:
-		{
-			RECT rect;
-			GetClientRect(hwnd, &rect);
-			render_state.width = rect.right - rect.left;
-			render_state.height = rect.bottom - rect.top;
+	case WM_SIZE:
+	{
+		RECT rect;
+		GetClientRect(hwnd, &rect);
+		render_state.width = rect.right - rect.left;
+		render_state.height = rect.bottom - rect.top;
 
-			int size = render_state.width * render_state.height * sizeof(unsigned int);
+		int size = render_state.width * render_state.height * sizeof(unsigned int);
 
-			if (render_state.memory) VirtualFree(render_state.memory, 0, MEM_RELEASE);
-			render_state.memory = VirtualAlloc(0, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+		if (render_state.memory) VirtualFree(render_state.memory, 0, MEM_RELEASE);
+		render_state.memory = VirtualAlloc(0, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
 
-			render_state.bitmap_info.bmiHeader.biSize = sizeof(render_state.bitmap_info.bmiHeader);
-			render_state.bitmap_info.bmiHeader.biWidth = render_state.width;
-			render_state.bitmap_info.bmiHeader.biHeight = render_state.height;
-			render_state.bitmap_info.bmiHeader.biPlanes = 1;
-			render_state.bitmap_info.bmiHeader.biBitCount = 32;
-			render_state.bitmap_info.bmiHeader.biCompression = BI_RGB;
-		}break;
-		case WM_CLOSE:
-		case WM_DESTROY:
-		{
-			running = false;
-		}break;
+		render_state.bitmap_info.bmiHeader.biSize = sizeof(render_state.bitmap_info.bmiHeader);
+		render_state.bitmap_info.bmiHeader.biWidth = render_state.width;
+		render_state.bitmap_info.bmiHeader.biHeight = render_state.height;
+		render_state.bitmap_info.bmiHeader.biPlanes = 1;
+		render_state.bitmap_info.bmiHeader.biBitCount = 32;
+		render_state.bitmap_info.bmiHeader.biCompression = BI_RGB;
+	}break;
+	case WM_CLOSE:
+	case WM_DESTROY:
+	{
+		running = false;
+	}break;
 
-		default:
-			result = DefWindowProc(hwnd, uMsg, wParam, lParam);
+	default:
+		result = DefWindowProc(hwnd, uMsg, wParam, lParam);
 	}
 	return result;
 }
 
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
+	float object_row_half_size_x[] = { 15.f,
+										10.f,
+										20.f };
+
+	u32 object_row_color[] = { 0x0000ff,
+								0x00ff00,
+								0xffff00 };
+
+	objectToDraw enemyObjects[] = { {addEnemyObject(-110.f,-35.f,1,1)},
+									{addEnemyObject(-90.f,-25.f,2,2)},
+									{addEnemyObject(-70.f,-15.f,3,3)},
+									{addEnemyObject(-50.f,-5.f,1,1)},
+									{addEnemyObject(-30.f,5.f,2,2)},
+									{addEnemyObject(-10.f,15.f,3,3)},
+									{addEnemyObject(10.f,25.f,1,1)},
+									{addEnemyObject(30.f,35.f,2,2)}
+	};
+
 	WNDCLASS window_class = {};
 	window_class.style = CS_HREDRAW | CS_VREDRAW;
 	window_class.lpszClassName = "Game Window";
@@ -101,10 +119,10 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 		{
 			switch (message.message)
 			{
-				case WM_KEYUP:
-				case WM_KEYDOWN: {
-					u32 vk_code = (u32)message.wParam;
-					bool is_down = ((message.lParam & (1 << 31)) == 0);
+			case WM_KEYUP:
+			case WM_KEYDOWN: {
+				u32 vk_code = (u32)message.wParam;
+				bool is_down = ((message.lParam & (1 << 31)) == 0);
 
 #define process_button(b, vk)\
 case vk: {\
@@ -112,29 +130,29 @@ input.buttons[b].changed = is_down != input.buttons[b].is_down;\
 input.buttons[b].is_down = is_down;\
 } break;
 
-					switch (vk_code) 
-					{
-						process_button(BUTTON_UP, VK_UP);
-						process_button(BUTTON_DOWN, VK_DOWN);
-						process_button(BUTTON_LEFT, VK_LEFT);
-						process_button(BUTTON_RIGHT, VK_RIGHT);
-					}
-
-				}break;
-				default:
+				switch (vk_code)
 				{
-					TranslateMessage(&message);
-					DispatchMessage(&message);
+					process_button(BUTTON_UP, VK_UP);
+					process_button(BUTTON_DOWN, VK_DOWN);
+					process_button(BUTTON_LEFT, VK_LEFT);
+					process_button(BUTTON_RIGHT, VK_RIGHT);
 				}
+
+			}break;
+			default:
+			{
+				TranslateMessage(&message);
+				DispatchMessage(&message);
+			}
 			}
 		}
 
 		//Simulate
-		simulate_game(&input, delta_time);
+		simulate_game(&input, delta_time, object_row_half_size_x, object_row_color, enemyObjects);
 
 		//Draw
 		StretchDIBits(hdc, 0, 0, render_state.width, render_state.height, 0, 0, render_state.width, render_state.height, render_state.memory, &render_state.bitmap_info, DIB_RGB_COLORS, SRCCOPY);
-	
+
 		LARGE_INTEGER frame_end_time;
 		QueryPerformanceCounter(&frame_end_time);
 		delta_time = (float)(frame_end_time.QuadPart - frame_begin_time.QuadPart) / performance_frequency;
